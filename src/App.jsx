29147887e5d5d8d225458fe2aa74ea7f2e3dfe1f
Ernesto Saiz <165823215+esaiz10@users.noreply.github.com>
2024-12-10 12:39:25 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Vite + React Ernesto</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+         Ernesto says, &quot;Hello there!&quot;
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
